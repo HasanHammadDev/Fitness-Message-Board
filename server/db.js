0ef5bb202f381883edb8f819e3512f7ed855d2da
Hasan Hammad <hasanj2k@gmail.com>
2024-06-26 19:22:25 -0400

    Updates db.js

--- server/db.js
+++ server/db.js
@@ -2,15 +2,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Replace with your MongoDB URI
 const uri = process.env.URI;
 
 const connectDB = async () => {
    try {
-      await mongoose.connect(uri, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true,
-      });
+      await mongoose.connect(uri);
       console.log('MongoDB connected');
    } catch (error) {
       console.error('Error connecting to MongoDB:', error.message);
