780ff9174b8087819a3ccc142bc373493bf0d4b7
Hasan Hammad <hasanj2k@gmail.com>
2024-06-28 13:31:00 -0400

    Added a retry button when editing message fails

--- src/components/EditMessage.jsx
+++ src/components/EditMessage.jsx
@@ -1,4 +1,4 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getMessage, editMessage } from "../MessageApi";
@@ -12,6 +12,8 @@ function EditMessage() {
     messageText: "",
   });
   const [error, setError] = useState(false);
+  const [retry, setRetry] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   function handleChange(name, value) {
     setInputs((values) => ({ ...values, [name]: value }));
@@ -36,10 +38,17 @@ function EditMessage() {
 
   useEffect(() => {
     async function loadMessage() {
+      if (retry) {
+        setIsLoading(true);
+      }
       const message = await getMessage(messageId);
       if (message.error) {
         setError(true);
+        setRetry(false);
+        setIsLoading(false);
       } else {
+        setIsLoading(false);
+        setRetry(false);
         setInputs({
           username: message.username,
           title: message.title,
@@ -49,46 +58,58 @@ function EditMessage() {
     }
 
     loadMessage();
-  }, [messageId]);
+  }, [messageId, retry]);
 
   return (
     <>
-    {error ? <div className="d-flex justify-content-center"><h1 className="error-heading">There was an error loading the message you're trying to edit</h1></div> : <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            value={inputs.username}
-            onChange={(e) => handleChange("username", e.target.value)}
-          />
-        </Form.Group>
+    {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <Spinner />
+        </div>
+      ) : <> {error ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <h1 className="error-heading">
+            There was an error loading the message you're trying to edit
+          </h1>
+          <Button onClick={() => setRetry(true)}>Retry</Button>
+        </div>
+      ) : (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              value={inputs.username}
+              onChange={(e) => handleChange("username", e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="title">
-          <Form.Label>Title</Form.Label>
-          <Form.Control
-            type="text"
-            value={inputs.title}
-            onChange={(e) => handleChange("title", e.target.value)}
-          />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              value={inputs.title}
+              onChange={(e) => handleChange("title", e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="messageText">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={3}
-            value={inputs.messageText}
-            onChange={(e) => handleChange("messageText", e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="me-2">
-          Save
-        </Button>
-        <Button variant="secondary" type="button" onClick={goBack}>
-          Cancel
-        </Button>
-      </Form>}
-      
+          <Form.Group className="mb-3" controlId="messageText">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={inputs.messageText}
+              onChange={(e) => handleChange("messageText", e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="me-2">
+            Save
+          </Button>
+          <Button variant="secondary" type="button" onClick={goBack}>
+            Cancel
+          </Button>
+        </Form>
+      )}</>}
     </>
   );
 }
