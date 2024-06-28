93c88e8cafd6d41fa64f4605cdf3bb9dc62f1245
Hasan Hammad <hasanj2k@gmail.com>
2024-06-28 00:42:49 -0400

    Added a retry button when loading messages fails

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import { getMessages } from "../MessageApi";
 import FilteredList from "./FilteredList";
@@ -8,8 +8,10 @@ function FilterComponent() {
   const filterParam = useLocation();
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [fetchedMessages, setFetchedMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
   const categories = ["Push", "Pull", "Legs", "Nutrition"];
+  const [retry, setRetry] = useState(false);
 
   const handleSelect = (category) => {
     if (selectedCategories.includes(category)) {
@@ -32,56 +34,77 @@ function FilterComponent() {
 
   useEffect(() => {
     async function getAllMessages() {
+      if(retry) {
+        setIsLoading(true);
+        setError(false)
+      }
       const messages = await getMessages();
-      if(messages.error) {
-        setError(true)
+      if (messages.error) {
+        setIsLoading(false);
+        setError(true);
+        setRetry(false);
       } else {
+        setIsLoading(false);
+        setRetry(false);
+        setError(false);
         setFetchedMessages(messages);
       }
     }
     getAllMessages();
-  }, []);
+  }, [retry]);
 
   return (
     <>
-      <div className="d-flex justify-content-center align-items-center">
-        {filterParam.pathname === "/filtered" ? (
-          categories.map((category, index) => (
-            <Button
-              className="category-button"
-              key={index}
-              value={category}
-              checked={selectedCategories.includes(index)}
-              onClick={() => handleSelect(category)}
-              variant={
-                selectedCategories.includes(category) ? "primary" : "secondary"
-              }
-            >
-              {category}{" "}
-              <span
-                className={
-                  selectedCategories.includes(category)
-                    ? "d-inline fw-bold"
-                    : "d-none"
+      {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center align-items-center">
+          {filterParam.pathname === "/filtered" ? (
+            categories.map((category, index) => (
+              <Button
+                className="category-button"
+                key={index}
+                value={category}
+                checked={selectedCategories.includes(index)}
+                onClick={() => handleSelect(category)}
+                variant={
+                  selectedCategories.includes(category) ? "primary" : "secondary"
                 }
               >
-                &#x2713;
-              </span>
+                {category}{" "}
+                <span
+                  className={
+                    selectedCategories.includes(category)
+                      ? "d-inline fw-bold"
+                      : "d-none"
+                  }
+                >
+                  &#x2713;
+                </span>
+              </Button>
+            ))
+          ) : null}
+
+          <div className="beacon">
+            <Button className="filter-button" onClick={handleFilter}>
+              Filter &#x25bc;
             </Button>
-          ))
-        ) : (
-          <></>
-        )}
-
-        <div className="beacon">
-          <Button className="filter-button" onClick={handleFilter}>
-            Filter &#x25bc;
-          </Button>
+          </div>
         </div>
+      )}
 
-      </div>
-       
-       {error ? <h1 className="error-heading">There was an error fetching messages.</h1> : <FilteredList filteredMessages={fetchedMessages} />}
+      {error ? (
+        <div className="d-flex align-items-center justify-content-center">
+          <h1 className="error-heading">
+            There was an error fetching messages.
+          </h1>
+          <Button onClick={() => setRetry(true)}>Retry</Button>
+        </div>
+      ) : (
+        <FilteredList filteredMessages={fetchedMessages} />
+      )}
     </>
   );
 }
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -8,6 +8,7 @@ function MessageList() {
   const [messages, setMessages] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [retry, setRetry] = useState(false);
 
   function deleteFromList(messageId) {
     // Delete message from database
@@ -21,53 +22,60 @@ function MessageList() {
 
   useEffect(() => {
     async function getAllMessages() {
+      if (retry) {
+        setIsLoading(true);
+      }
       const messages = await getMessages();
-      console.log(messages.error)
-      if(messages.error) {
-        setError(true)
-        setIsLoading(false)
+      if (messages.error) {
+        setError(true);
+        setIsLoading(false);
+        setRetry(false);
       } else {
+        setError(false);
         setMessages(messages);
         setIsLoading(false);
-    }
-
+        setRetry(false);
+      }
     }
 
     getAllMessages();
-  }, []);
+  }, [retry]);
 
   return (
     <>
-       {isLoading ? (
-          <div className="d-flex justify-content-center align-items-center">
-             <Spinner />
-          </div>
-       ) : (
-          <>
-             {error ? (
-                <div className="d-flex align-items-center justify-content-center">
-                   <h1 className="error-heading">There was an error fetching messages.</h1>
-                </div>
-             ) : (
-                <>
-                   <div className="d-flex justify-content-center align-items-center">
-                      <Link className="beacon" to="/filtered">
-                         <Button className="filter-button">Filter &#x25bc;</Button>
-                      </Link>
-                   </div>
-                   {messages.map((message) => (
-                      <Message
-                         key={message._id}
-                         value={message}
-                         delete={deleteFromList}
-                      />
-                   ))}
-                </>
-             )}
-          </>
-       )}
+      {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          {error ? (
+            <div className="d-flex align-items-center justify-content-center">
+              <h1 className="error-heading">
+                There was an error fetching messages.
+              </h1>
+              <Button onClick={() => setRetry(true)}>Retry</Button>
+            </div>
+          ) : (
+            <>
+              <div className="d-flex justify-content-center align-items-center">
+                <Link className="beacon" to="/filtered">
+                  <Button className="filter-button">Filter &#x25bc;</Button>
+                </Link>
+              </div>
+              {messages.map((message) => (
+                <Message
+                  key={message._id}
+                  value={message}
+                  delete={deleteFromList}
+                />
+              ))}
+            </>
+          )}
+        </>
+      )}
     </>
- );
+  );
 }
 
 export default MessageList;
